37d0cb50477fd584ded15bb56d540a33d72c238c
Irfanullah <irfan@khu.ac.kr>
2021-09-23 15:11:30 +0900

    Update model.py
    comments

--- Python/KHopGCN/pygcn/model.py
+++ Python/KHopGCN/pygcn/model.py
@@ -16,15 +16,18 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
-
+   
     def get_weights(self):
+         # get weights of a current model
         return {k: v.cpu() for k, v in self.state_dict().items()}
 
     def set_weights(self, weights):
+         # set weights of a current model
         self.load_state_dict(weights)
 
     def get_gradients(self):
         grads = []
+        # get gradients 
         for p in self.parameters():
             grad = None if p.grad is None else p.grad.data.cpu().numpy()
             grads.append(grad)
